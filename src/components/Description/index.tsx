d2a0351f8e5aec932aaaca117c1c58536ff904b6
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-06-06 21:25:28 -0300

    Componentizando descrição

---
+++ src/components/Description/index.tsx
@@ -0,0 +1,233 @@
+import React from 'react';
+import { PokeDTO } from '../../dtos/PokeDTO';
+
+import WeightIcon from '../../assets/WeightIcon.svg';
+import HeightIcon from '../../assets/HeightIcon.svg';
+import MaleIcon from '../../assets/MaleIcon.svg';
+import FemaleIcon from '../../assets/FemaleIcon.svg';
+
+import {
+  DescriptionPokemonContainer,
+  DescriptionText,
+  InformationText,
+  PhysicalCharacteristicsContainer,
+  PhysicalCharacteristicsWrap,
+  PhysicalInfoWrap,
+  PhysicalLogo,
+  PhysicalTextTitle,
+  PhysicalSubTitle,
+  SeparatorWidget,
+  FeaturesContainer,
+  TitleFeatures,
+  GengerWrap,
+  GengerTitle,
+  GengerType,
+  GengerTypeIcon,
+  GengerPercentage,
+  PokemonStatusContainer,
+  PokemonStatusWrap,
+  PokemonStatusTitle,
+  PokemonStatusNumber,
+  PokemonStatusBarWrap,
+  PokemonStatusInactive,
+  PokemonStatusActive,
+} from './styles';
+
+interface Props {
+  pokemon: PokeDTO;
+}
+
+export function Description({ pokemon } : Props){
+
+  const totalBaseStat = 
+  pokemon.stats[0].base_stat +
+  pokemon.stats[1].base_stat +
+  pokemon.stats[2].base_stat +
+  pokemon.stats[3].base_stat +
+  pokemon.stats[4].base_stat +
+  pokemon.stats[5].base_stat;
+
+  const weightPokemon = ((pokemon.weight)/10);
+  const heightPokemon = ((pokemon.height)/10);
+
+  const hp = ((pokemon.stats[0].base_stat));
+  const attack= ((pokemon.stats[1].base_stat));
+  const defense = ((pokemon.stats[2].base_stat));
+  const specialAttack = ((pokemon.stats[3].base_stat));
+  const specialDefense = ((pokemon.stats[4].base_stat));
+  const speed = ((pokemon.stats[5].base_stat));
+  const totalStats = totalBaseStat / 4;
+
+  const color = pokemon.types[0].type.name;
+
+  return (
+    <DescriptionPokemonContainer>
+      <DescriptionText color={color}>
+        Descrição
+      </DescriptionText>
+
+      <InformationText>
+        Bulbasaur pode ser visto cochilando sob a luz do sol. Há uma semente nas costas. Ao absorver os raios do sol, a semente cresce progressivamente maior
+      </InformationText>
+
+      <PhysicalCharacteristicsContainer>
+
+        <PhysicalCharacteristicsWrap>
+          <PhysicalInfoWrap>
+            <PhysicalLogo>
+              <WeightIcon
+                width={16}
+                height={16}
+              />
+            </PhysicalLogo>
+            <PhysicalTextTitle>{weightPokemon} kg</PhysicalTextTitle>
+          </PhysicalInfoWrap>
+          <PhysicalSubTitle>Peso</PhysicalSubTitle>
+        </PhysicalCharacteristicsWrap>
+
+        <SeparatorWidget></SeparatorWidget>
+
+        <PhysicalCharacteristicsWrap>
+          <PhysicalInfoWrap>
+            <PhysicalLogo>
+              <HeightIcon
+                width={8}
+                height={16}
+              />
+            </PhysicalLogo>
+            <PhysicalTextTitle>{heightPokemon} m</PhysicalTextTitle>
+          </PhysicalInfoWrap>
+          <PhysicalSubTitle>Altura</PhysicalSubTitle>
+        </PhysicalCharacteristicsWrap>
+
+        <SeparatorWidget></SeparatorWidget>
+
+        <PhysicalCharacteristicsWrap>
+          <PhysicalInfoWrap>
+            <PhysicalTextTitle>{pokemon.moves[0].move.name}</PhysicalTextTitle>
+          </PhysicalInfoWrap>
+          <PhysicalSubTitle>Poder Principal</PhysicalSubTitle>
+        </PhysicalCharacteristicsWrap>
+
+      </PhysicalCharacteristicsContainer>
+
+      <FeaturesContainer>
+        <TitleFeatures>Suas características</TitleFeatures>
+
+        <GengerWrap>
+          <GengerTitle>Gênero</GengerTitle>
+
+          <GengerType>
+            <GengerTypeIcon>
+              <MaleIcon 
+                width={17}
+                height={14}
+              />
+            </GengerTypeIcon>
+            <GengerPercentage>87,5%</GengerPercentage>
+          </GengerType>
+
+          <GengerType>
+            <GengerTypeIcon>
+              <FemaleIcon 
+                width={17}
+                height={14}
+              />
+            </GengerTypeIcon>
+            <GengerPercentage>12,5%</GengerPercentage>
+          </GengerType>
+        </GengerWrap>
+
+        <PokemonStatusContainer>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>Saúde</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[0].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={hp} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>Ataque</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[1].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={attack} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>Defesa</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[2].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={defense} color={color}>  
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>VI. Ataque</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[3].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={specialAttack} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>VI. Defesa</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[4].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={specialDefense} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>Velocidade</PokemonStatusTitle>
+          <PokemonStatusNumber>{pokemon.stats[5].base_stat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={speed} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        <PokemonStatusWrap>
+          <PokemonStatusTitle>Total</PokemonStatusTitle>
+          <PokemonStatusNumber>{totalBaseStat}</PokemonStatusNumber>
+
+          <PokemonStatusBarWrap>
+            <PokemonStatusInactive></PokemonStatusInactive>
+            <PokemonStatusActive 
+              percentage={totalStats} color={color}>
+            </PokemonStatusActive>
+          </PokemonStatusBarWrap>
+        </PokemonStatusWrap>
+
+        </PokemonStatusContainer>
+
+      </FeaturesContainer>
+
+    </DescriptionPokemonContainer>
+  );
+}
---
+++ src/components/Description/styles.ts
@@ -0,0 +1,275 @@
+import styled from 'styled-components/native';
+
+interface ColorProps {
+  color: string;
+};
+
+interface PokemonStatusProps {
+  percentage: number;
+  color: string;
+};
+
+export const DescriptionPokemonContainer = styled.View`
+  flex: 1;
+  margin-top: -40px;
+  border-radius: 40px;
+  background-color: ${({ theme }) => theme.colors.text_light};
+  padding-top: 52px;
+  padding-bottom: 52px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const DescriptionText = styled.Text<ColorProps>`
+  font-size: 20px;
+  font-weight: 700;
+
+  ${({ theme, color }) => 
+    color == 'grass' && ({ color: theme.colors.grass})};
+
+  ${({ theme, color }) => 
+    color == 'fire' && ({ color: theme.colors.fire})};
+
+  ${({ theme, color }) => 
+    color == 'water' && ({ color: theme.colors.water})};
+
+  ${({ theme, color }) => 
+    color == 'electric' && ({ color: theme.colors.electric})};
+
+  ${({ theme, color }) => 
+    color == 'bug' && ({ color: theme.colors.bug})};
+
+  ${({ theme, color }) => 
+    color == 'rock' && ({ color: theme.colors.rock})};
+
+  ${({ theme, color }) => 
+    color == 'ghost' && ({ color: theme.colors.ghost})};
+
+  ${({ theme, color }) => 
+    color == 'steel' && ({ color: theme.colors.steel})};
+
+  ${({ theme, color }) => 
+    color == 'psychic' && ({ color: theme.colors.psychic})};
+
+  ${({ theme, color }) => 
+    color == 'ice' && ({ color: theme.colors.ice})};
+
+  ${({ theme, color }) => 
+    color == 'dark' && ({ color: theme.colors.dark})};
+
+  ${({ theme, color }) => 
+    color == 'fairy' && ({ color: theme.colors.fairy})};
+
+  ${({ theme, color }) => 
+    color == 'normal' && ({ color: theme.colors.normal})};
+
+  ${({ theme, color }) => 
+    color == 'fighting' && ({ color: theme.colors.fighting})};
+
+  ${({ theme, color }) => 
+    color == 'flying' && ({ color: theme.colors.flying})};
+
+  ${({ theme, color }) => 
+    color == 'poison' && ({ color: theme.colors.poison})};
+
+  ${({ theme, color }) => 
+    color == 'ground' && ({ color: theme.colors.ground})};
+
+  ${({ theme, color }) => 
+    color == 'dragon' && ({ color: theme.colors.dragon})};
+`;
+
+export const InformationText = styled.Text`
+  margin-top: 41px;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const PhysicalCharacteristicsContainer= styled.View`
+  margin-top: 32px;
+  width: 100%;
+  padding-right: 20px;
+  flex-direction: row;
+`;
+
+export const PhysicalCharacteristicsWrap= styled.View`
+  width: 33%;
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PhysicalInfoWrap = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PhysicalLogo = styled.View`
+  width: 16px;
+  height: 16px;
+  align-items: center;
+  margin-right: 8px;
+`;
+
+export const PhysicalTextTitle = styled.Text`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.text_dark};
+  text-align: center;
+  text-transform: capitalize;
+`;
+
+export const PhysicalSubTitle = styled.Text`
+  font-size: 12px;
+  height: 16px;
+  color: #767676;
+`;
+
+export const SeparatorWidget = styled.View`
+  height: 100%;
+  width: 0.5%;
+  background-color: #E0E0E0;
+`;
+
+export const FeaturesContainer = styled.View`
+  margin-top: 31px;
+`;
+
+export const TitleFeatures = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const GengerWrap = styled.View`
+  flex-direction: row;
+  margin-top: 17px;
+`;
+
+export const GengerTitle = styled.Text`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.text_dark};
+  width: 88px;
+`;
+
+export const GengerType = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 68px;
+`;
+
+export const GengerTypeIcon = styled.View`
+  margin-right: 4px;
+  width: 17px;
+  height: 14px;
+`;
+
+export const GengerPercentage = styled.Text`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const PokemonStatusContainer = styled.View``;
+
+export const PokemonStatusWrap = styled.View`
+  margin-top: 14px;
+  flex-direction: row;
+`;
+
+export const PokemonStatusTitle = styled.Text`
+  width: 88px;
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const PokemonStatusNumber = styled.Text`
+  width: 30px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const PokemonStatusBarWrap = styled.View`
+  flex: 1;
+  margin-left: 15px;
+  margin-right: 15px;
+  align-items: flex-start;
+  justify-content: center;
+`;
+
+export const PokemonStatusInactive = styled.View`
+  width: 100%;
+  background-color: #F4F5F4;
+  height: 5px;
+  border-radius: 2.5px;
+`;
+
+export const PokemonStatusActive = styled.View<PokemonStatusProps>`
+  position: absolute;
+  height: 5px;
+  border-radius: 2.5px;
+
+  ${({ percentage }) => ({ width: percentage})};
+
+  ${({ theme, color }) => 
+    color == 'grass' && ({ backgroundColor: theme.colors.grass})};
+
+  ${({ theme, color }) => 
+    color == 'fire' && ({ backgroundColor: theme.colors.fire})};
+
+  ${({ theme, color }) => 
+    color == 'water' && ({ backgroundColor: theme.colors.water})};
+
+  ${({ theme, color }) => 
+    color == 'electric' && ({ backgroundColor: theme.colors.electric})};
+
+  ${({ theme, color }) => 
+    color == 'bug' && ({ backgroundColor: theme.colors.bug})};
+
+  ${({ theme, color }) => 
+    color == 'rock' && ({ backgroundColor: theme.colors.rock})};
+
+  ${({ theme, color }) => 
+    color == 'ghost' && ({ backgroundColor: theme.colors.ghost})};
+
+  ${({ theme, color }) => 
+    color == 'steel' && ({ backgroundColor: theme.colors.steel})};
+
+  ${({ theme, color }) => 
+    color == 'psychic' && ({ backgroundColor: theme.colors.psychic})};
+
+  ${({ theme, color }) => 
+    color == 'ice' && ({ backgroundColor: theme.colors.ice})};
+
+  ${({ theme, color }) => 
+    color == 'dark' && ({ backgroundColor: theme.colors.dark})};
+
+  ${({ theme, color }) => 
+    color == 'fairy' && ({ backgroundColor: theme.colors.fairy})};
+
+  ${({ theme, color }) => 
+    color == 'normal' && ({ backgroundColor: theme.colors.normal})};
+
+  ${({ theme, color }) => 
+    color == 'fighting' && ({ backgroundColor: theme.colors.fighting})};
+
+  ${({ theme, color }) => 
+    color == 'flying' && ({ backgroundColor: theme.colors.flying})};
+
+  ${({ theme, color }) => 
+    color == 'poison' && ({ backgroundColor: theme.colors.poison})};
+
+  ${({ theme, color }) => 
+    color == 'ground' && ({ backgroundColor: theme.colors.ground})};
+
+  ${({ theme, color }) => 
+    color == 'dragon' && ({ backgroundColor: theme.colors.dragon})};
+
+`;
